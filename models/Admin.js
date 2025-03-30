b5e6f17c352888d403791c271ad024c30abb2cfb
JoseMandujanoCarrillo <mandujanocarrillo27@gmail.com>
2025-03-30 17:10:06 -0600

    first commit

--- models/Admin.js
+++ models/Admin.js
@@ -1,44 +1,5 @@
 const mongoose = require('mongoose');
 
-const AdminSchema = new mongoose.Schema({
-  correo: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  contrasena: {
-    type: String,
-    required: true,
-  },
-  nombre: {
-    type: String,
-    required: true,
-  },
-  fotoPerfil: {
-    type: String,
-    default: null,
-  },
-  peso: {
-    type: Number,
-    default: null,
-  },
-  altura: {
-    type: Number,
-    default: null,
-  },
-  fechaNacimiento: {
-    type: Date,
-    required: true,
-  },
-  genero: {
-    type: String,
-    required: true,
-  },
-}, { timestamps: true });
-
-// Especificar el nombre de la colección como "Usuario"
-const Usuario = mongoose.model('Usuario', usuarioSchema, 'Usuario');
-
 // Crear el esquema para Admin
 const adminSchema = new mongoose.Schema({
   correo: {
